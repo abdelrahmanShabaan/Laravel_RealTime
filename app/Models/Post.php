2df9acb8b0282ea4df309226143a4b81c333b1b4
abdelrahman <boodyelmasry6@gmail.com>
2022-09-15 16:56:07 +0200

    first commit

---
+++ app/Models/Post.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Post extends Model
+{
+
+     protected $table = "posts";
+     protected $fillable = ['title','content','user_id','created_at','update_at'];
+     protected $hidden = ['created_at','update_at'];
+     public $timestamps =true;
+
+
+    public function user()
+    {
+        return $this->belongsTo('App\User', 'user_id', 'id');
+    }
+
+
+    public function comments()
+    {
+        return $this->hasMany('App\Models\Comment', 'post_id', 'id');
+    }
+
+}
---
+++ resources/views/home.blade.php
@@ -0,0 +1,55 @@
+﻿﻿@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">Dashboard</div>
+
+                    @if(Session::has('success'))
+                        <div class="col-6 alert alert-success justify-content-center d-flex">
+                            {{Session::get('success')}}
+                        </div>
+                    @endif
+                    @if(isset($posts) && $posts -> count() > 0)
+                        @foreach($posts as $post)
+                            <div class="card-body">
+                                @if (session('status'))
+                                    <div class="alert alert-success" role="alert">
+                                        {{ session('status') }}
+                                    </div>
+                                @endif
+                                <h1> {{$post -> title}} - @if(Auth::id() == $post -> user -> id)   المالك @endif</h1>
+                                <br>
+                                {{$post -> content}}
+
+                                <br>
+                                <br>
+                                <form method="POST" action="{{route('comment.save')}}" enctype="multipart/form-data">
+                                    @csrf
+
+                                    <input type="hidden" name="post_id" value="{{$post -> id}}">
+                                    <div class="form-group">
+                                        <input type="text" class="form-control" name="post_content">
+                                        @error('name_ar')
+                                        <small class="form-text text-danger">{{$message}}</small>
+                                        @enderror
+                                    </div>
+
+
+                                    @if(Auth::id() != $post -> user -> id)
+                                        <button type="submit" class="btn btn-primary">أضافه ردك</button>
+                                    @endif
+                                </form>
+
+
+                            </div>
+                        @endforeach
+                    @endif
+
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
